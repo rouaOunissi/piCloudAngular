58faf0d0d0ef5919c4bd8e9fead8d6f905c86644
rouaOunissi <roua.ounissi@esprit.tn>
2024-04-28 21:21:07 +0100

    localStorage

--- src/app/modules/front-office/components/sub-components/ressource-add/ressource-add.component.ts
+++ src/app/modules/front-office/components/sub-components/ressource-add/ressource-add.component.ts
@@ -13,6 +13,8 @@ export class RessourceAddComponent implements OnInit {
   ressourceTypes: any[] = [];
   selectedFile: File | null = null;
   fileName: string = 'Choose file'; 
+  userId: number | null = null;
+
 
  
  
@@ -27,40 +29,53 @@ export class RessourceAddComponent implements OnInit {
 
 
 
-ngOnInit(): void {this.getRessourceTypes();}
+ngOnInit(): void {this.getRessourceTypes();
+    // Récupérer l'id de l'utilisateur depuis le localStorage
+    const userIdFromStorage = localStorage.getItem('userId');
+    console.log('User ID from storage:', userIdFromStorage);
+    if (userIdFromStorage) {
+        this.userId = parseInt(userIdFromStorage, 10);
+        console.log('Parsed user ID:', this.userId);
+    }
+
+}
 
 
 
 addRessource(): void {
   const formData = new FormData();
   if (this.selectedFile) {
-    formData.append('file', this.selectedFile);  }
-  formData.append('ressource', JSON.stringify(this.ress));
-    
-  const userId = localStorage.getItem('userId');
-  if (!userId) {
-    console.error('User ID not found in localStorage');
-    return;
+    formData.append('file', this.selectedFile);
   }
-  
-  formData.append('idUser', userId); 
+   // Pass userId along with ressource object
+   const ressourceData = { ...this.ress, idUser: this.userId };
+
+   formData.append('ressource', JSON.stringify(ressourceData));
+
+  // Logging userId to ensure it's not null
+  console.log('UserID before appending to formData:', this.userId);
+
+  // Ajouter l'id de l'utilisateur au formulaire
+  if (this.userId !== null) {
+    formData.append('idUser', this.userId.toString()); 
+  }
 
   this.http.post('http://localhost:8060/api/v1/ressource/uploadRessData', formData)
     .subscribe(
       (response: any) => { 
         console.log('Ressource added successfully:', response);
-        alert("Ressource added successfully!");
+      
         this.ress = {};
         this.fileName = ''; 
-      
+         // Rediriger vers la table des ressources
         this.router.navigate(['/front/main/ressource']);
       },
       (error: any) => { 
         console.error('Error adding ressource:', error);
-        alert("Error adding ressource. Please try again!");
       }
     );
 }
+
 
 
 
